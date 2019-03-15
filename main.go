789ad45f844ed157ba4ecba9744fceae4f277d05
mcclayac <anthony.mcclay@me.com>
2019-03-15 02:36:56 -0500

    Updted commands for ECR and Update the port to 9090

--- main.go
+++ main.go
@@ -3,7 +3,7 @@ package main
 import "net/http"
 
 func helloWorld(w http.ResponseWriter, r *http.Request) {
-	w.Write([]byte("Hello World\n\nThese Nutz \n\n goService"))
+	w.Write([]byte("Hello World\n\nThese Nutz \n\n goService\n\nDeveloper = mcclayac\n\n mcclay-goapp"))
 }
 
 func main() {
